219029e877b356cd1862e438ccdd0f61051d6b40
Tim Leuschner <tim.leuschner@gmail.com>
2024-09-28 19:23:56 +0200

    style: format MapComponent for improved readability

--- src/MapComponent.tsx
+++ src/MapComponent.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 
 interface MapComponentProps {
   startPoint: { lat: number; lng: number };
   endPoint: { lat: number; lng: number };
 }
 
-const MapComponent: React.FC<MapComponentProps> = ({ startPoint, endPoint }) => {
+const MapComponent: React.FC<MapComponentProps> = ({
+  startPoint,
+  endPoint,
+}) => {
   return (
-    <MapContainer center={startPoint} zoom={13} style={{ height: "400px", width: "100%" }}>
+    <MapContainer
+      center={startPoint}
+      zoom={13}
+      style={{ height: "400px", width: "100%" }}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
