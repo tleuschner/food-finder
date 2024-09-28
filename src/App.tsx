be42950f08af49d638914fb8588dcffec95bd0ad
Tim Leuschner <tim.leuschner@gmail.com>
2024-09-28 19:00:46 +0200

    refactor: remove unused elements and simplify the App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -62,23 +62,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
       <div className="card">
         <button onClick={requestLocation}>Request Location</button>
         {location ? (
@@ -99,7 +82,6 @@ function App() {
           <p>Loading location...</p>
         )}
       </div>
-      <p>Click on the Vite and React logos to learn more</p>
     </>
   );
 }
